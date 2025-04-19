fa8b77dc0ba5ef31a48745f928af59d1e9e3d426
YL - Jun Senido <plukyljunsenido@gmail.com>
2025-04-19 22:16:02 +0800

    Adding Roles Admin and User

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => RoleMiddleware::class, // Register role middleware
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
