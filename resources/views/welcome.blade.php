3b2e695eea8de66f2e7ae0eb36e1f8fd5b033b88
YL - Jun Senido <plukyljunsenido@gmail.com>
2025-04-27 19:00:19 +0800

    Changing Login Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,23 +17,43 @@
             
         @endif
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col" style="background-image: url('/images/background_full.jpg'); background-size: cover;">
-        
-        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-5xl lg:flex-row">
-                <div class="h-screen w-full flex">
-                    <div class="h-full w-1/2 bg-white" >
-                        <div>
-                            Hello
-                        </div>
-                    </div>
-                    <div class="bg-red-600 w-1/2">
-                        <div class="bg-yellow-400 ">
-                            World
-                        </div>
-                    </div>
+    <body class="h-screen w-screen bg-cover bg-center" style="background-image: url('/images/background.jpg');">
+        <div class="h-full w-full bg-black/50 flex items-center justify-center">
+            <div class="flex flex-col md:flex-row w-11/12 md:w-8/12 lg:w-6/12 bg-white/20 rounded-lg p-8 backdrop-blur-sm">
+            <!-- Left Section -->
+            <div class="w-full md:w-1/2 mb-8 md:mb-0 md:pr-6 text-white">
+                <h1 class="text-4xl font-bold mb-4">Welcome Back</h1>
+                <p class="mb-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                <div class="flex space-x-4">
+                <a href="#"><img src="https://img.icons8.com/ios-glyphs/30/ffffff/facebook-new.png"/></a>
+                <a href="#"><img src="https://img.icons8.com/ios-glyphs/30/ffffff/twitter--v1.png"/></a>
+                <a href="#"><img src="https://img.icons8.com/ios-glyphs/30/ffffff/instagram-new.png"/></a>
+                <a href="#"><img src="https://img.icons8.com/ios-glyphs/30/ffffff/youtube-play.png"/></a>
                 </div>
-            </main>
+            </div>
+
+            <!-- Right Section (Form) -->
+            <div class="w-full md:w-1/2 bg-white/20 p-6 rounded-lg">
+                <h2 class="text-2xl font-semibold text-white mb-6">Sign in</h2>
+                <form class="flex flex-col space-y-2">
+                <input type="email" placeholder="Email Address" class="p-3 rounded bg-white/80 focus:outline-none">
+                <input type="password" placeholder="Password" class="p-3 rounded bg-white/80 focus:outline-none">
+                <div class="flex items-center space-x-2 text-white">
+                    <input type="checkbox" id="remember" class="accent-orange-500">
+                    <label for="remember">Remember Me</label>
+                </div>
+                <button class="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded">Login</button>
+                <button class="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded">Sign Up</button>
+                
+                <a href="#" class="text-sm text-white hover:underline text-center">Lost your password?</a>
+                <p class="text-xs text-white text-center mt-4">
+                    By clicking on "Sign in now" you agree to<br>
+                    <a href="#" class="underline">Terms of Service</a> | <a href="#" class="underline">Privacy Policy</a>
+                </p>
+                </form>
+            </div>
+
+            </div>
         </div>
 
         @if (Route::has('login'))
